426538b81d59c7a0be0d1915e66f0b45da938c87
Jean-Baptiste Lescher <jblescher@gmail.com>
2018-01-17 15:36:56 +0100

    2016 Day 03

--- 2016/day_03_squares_with_three_sides/s.py
+++ 2016/day_03_squares_with_three_sides/s.py
@@ -1,15 +1,38 @@
 #!/usr/bin/env python3
 
 
-def test_triangle(x, y, z):
-        return z < x + y
+import argparse
 
-with open('input') as f:
+
+def test_triangle(x, y, z):
+    '''
+    x, y, z are expected to be sorted
+    '''
+    return z < x + y
+
+
+def possible_triangles(triangle_list):
     possible_triangles = 0
-    l = list(map(lambda x: map(int, x.split()), list(f)))
-    for i in range(0, len(l), 3):
-        p = list(map(sorted, zip(*l[i:i+3])))
-        for t in p:
-            if test_triangle(*t):
-                possible_triangles += 1
-    print(possible_triangles)
+    sorted_triangles = [ sorted(x) for x in triangle_list ]
+    for t in sorted_triangles:
+        if test_triangle(*t):
+            possible_triangles += 1
+    return possible_triangles
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', nargs='?', default='input', type=str)
+    args = parser.parse_args()
+
+    with open('input') as f:
+        l = f.readlines()
+        triangles = [ tuple(map(int, x.rstrip().split())) for x in l ]
+
+    # zip by group of three
+    group3_triangles = []
+    for i in range(0, len(triangles), 3):
+        group3_triangles.extend(zip(*triangles[i:i+3]))
+
+    print('P1:', possible_triangles(triangles))
+    print('P2:', possible_triangles(group3_triangles))
