1b08fa3f33ed429a3736abb6cd5ad3813319b2b8
Jean-Baptiste Lescher <jblescher@gmail.com>
2018-01-18 15:48:23 +0100

    2016 Day 06

---
+++ 2016/day_06_signals_and_noise/s.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import argparse
+from collections import Counter
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', nargs='?', default='input', type=str)
+    args = parser.parse_args()
+
+    with open(args.input) as f:
+        messages = f.readlines()
+
+    messages = [ x.rstrip() for x in messages ]
+
+    letters = [ Counter(x).most_common(1).pop()[0] for x in zip(*messages) ]
+    print('P1:', ''.join(letters))
+
+    letters = [ Counter(x).most_common().pop()[0] for x in zip(*messages) ]
+    print('P2:', ''.join(letters))
+
+    
+
