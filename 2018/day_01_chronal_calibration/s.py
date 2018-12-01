ee567e303dd48a1845f9713c3e7567e56ff31195
Jean-Baptiste Lescher <jblescher@gmail.com>
2018-12-01 09:33:40 +0100

    2018 Day 01

---
+++ 2018/day_01_chronal_calibration/s.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import itertools
+import argparse
+
+def p2(freqs):
+    cur_freq = 0
+    previous_freqs = { cur_freq }
+    for f in itertools.cycle(freqs):
+        cur_freq += f
+        if cur_freq in previous_freqs:
+            return cur_freq
+        else:
+            previous_freqs.add(cur_freq)
+        
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', nargs='?', default='input', type=str)
+    args = parser.parse_args()
+
+    with open(args.input) as f:
+        freqs = [ int(l.rstrip()) for l in f]
+    print('P1:', sum(freqs))
+    print('P2:', p2(freqs))
