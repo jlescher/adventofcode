524cbd7b8ed8e10ff5fef3eed3cbea96a8391d61
Jean-Baptiste Lescher <jblescher@gmail.com>
2020-01-02 18:00:12 +0100

    2018 Day 16 part2 in progress

--- 2018/day_16_chronal_classification/s.py
+++ 2018/day_16_chronal_classification/s.py
@@ -3,6 +3,7 @@
 import argparse
 import re
 from pprint import pprint
+from copy   import deepcopy
 
 class Device:
     def __init__(self):
@@ -26,8 +27,14 @@ class Device:
                 self.eqrr,
                 ]
 
-    def execute(self, instruction):
-        opcode, A, B, C = instruction.split(' ')
+    def execute_prog(self, prog):
+        self.set_registers(0, 0, 0, 0)
+        print(self.registers)
+        if self.opcodes:
+            for opcode, A, B, C in prog:
+                print(self.opcodes[opcode].__name__, '(', A, B, C, ')')
+                self.opcodes[opcode](A, B, C)
+                print(self.registers)
 
     def set_registers(self, A, B, C, D):
         self.registers = [ A, B, C, D ]
@@ -80,6 +87,37 @@ class Device:
     def eqrr(self, A, B, C):
         self.registers[C] = 1 if self.registers[A] == self.registers[B] else 0
 
+    def map_instructions_to_opcodes(self, tests):
+        # Create a set for each opcode number
+        opcodes = [ set(self.instructions) for i in range(16)] 
+        for t in tests:
+            opcode = t[1][0]
+            is_valid = set()
+            for instruction in opcodes[opcode]:
+                self.set_registers(*t[0])
+                instruction(*t[1][1:])
+                if tuple(self.registers) == t[2]:
+                    is_valid.add(instruction)
+            opcodes[opcode] = is_valid
+
+        # Map unique opcodes and remove them from the other sets
+        self.opcodes = [ None for i in range(16) ]
+        while True:
+            found = False
+            # Loop through the sets and add unambiguous opcodes 
+            for opcode, instructions in enumerate(opcodes):
+                if len(instructions) == 1:
+                    instruction = instructions.pop()
+                    self.opcodes[opcode] = instruction
+                    found = True
+                    break
+            # Remove instructions[1] from the other sets
+            if found:
+                for instructions in opcodes:
+                    instructions.discard(instruction)
+            else:
+                break
+        assert len(set(self.opcodes)) == len(self.opcodes)
 
 def part1(tests):
     d = Device()
@@ -95,11 +133,12 @@ def part1(tests):
             cnt += 1
     return cnt
 
-def part2(tests):
+def part2(tests, prog):
     d = Device()
-    opcodes = [ dict() for instruction in d.instructions ]
-    pprint(opcodes)
-
+    d.map_instructions_to_opcodes(tests)
+    d.execute_prog(prog)
+    return d.registers[0]
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -123,4 +162,8 @@ if __name__ == '__main__':
     except IndexError:
         pass
     
+    # Extract part2 input
+    prog = [ tuple(map(int, l.strip().split(' '))) for l in lines[i+2:] ]
+
     print('part1: {}'.format(part1(tests)))
+    print('part2: {}'.format(part2(tests, prog)))
